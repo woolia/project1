4a273d43a1a5ad34ec7493b0d07522281b476584
woolia <76478538+woolia@users.noreply.github.com>
2021-09-29 17:45:00 +0900

    commit

--- src/test/java/project/pr/PrApplicationTests.java
+++ src/test/java/project/pr/PrApplicationTests.java
@@ -3,10 +3,10 @@ package project.pr;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PrApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
